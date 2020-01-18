e7c758bae6427b28d60115d1ca0c79ac208652e4
Sofia Kimpimäki <kimpims@aalto.fi>
2020-01-18 13:56:27 +0200

    Added navigation and login page

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
-import React from "react";
-import firebase from "firebase";
-import { config } from "./components/Firebase/firebase.js";
+import React from 'react';
+import firebase from 'firebase';
+import { config } from './components/Firebase/firebase.js';
+import Register from '../src/pages/register.js'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from 'react-router-dom';
+import Home from "./pages/home";
 
 firebase.initializeApp(config);
 
-var db = firebase.firestore();
-function App() {
-  return (
-    <div>
-      <button
-        onClick={() =>
-          db
-            .collection("matches")
-            .get()
-            .then(querySnapshot => {
-              querySnapshot.forEach(doc => {
-                console.log(doc.data());
-              });
-            })
-        }
-      >
-        CLICK ME
-      </button>
-    </div>
-  );
+class App extends React.Component {
+    render() {
+        return (
+            <Router>
+                    <Switch>
+                        <Route key='home' exact path='/' component={Home}/>
+                        <Route key='register' exact path='/register' component={Register}/>
+                    </Switch>
+            </Router>
+        );
+    }
 }
 
 export default App;
