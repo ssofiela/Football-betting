0c0671459c8b22379244a7697fd148a220e7c59b
Sofia Kimpimäki <kimpims@aalto.fi>
2020-01-20 18:53:40 +0200

    Added bets to the local state

--- src/components/BetList.js
+++ src/components/BetList.js
@@ -3,9 +3,25 @@ import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import {makeStyles} from "@material-ui/core";
 import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 const BetList = state => {
     const styles = useStyles();
+
+    const [firstBets, setFirstBets] = React.useState([]);
+    const [secondBets, setSecondBets] = React.useState([]);
+
+
+    const handleFirst = (bet, i) => {
+        const firstList = firstBets;
+        firstList[i] = bet;
+        setFirstBets(firstList)
+    };
+    const handleSecond = (bet, i) => {
+        const secondList = secondBets;
+        secondList[i] = bet;
+        setSecondBets(secondList)
+    };
 
     const listGroups = () => {
         const groupJSX = [];
@@ -18,13 +34,16 @@ const BetList = state => {
                     <Grid container direction="row" justify="space-evenly" key={i}>
                         <form className={styles.root} noValidate autoComplete="off">
                             <div>
+                                <a>
                                 {state.state.location.state.matches[i].away}
+                                </a>
                                 <TextField
                                     required
                                     name="bet"
                                     type="number"
                                     id="bet"
                                     inputProps={{min: 0, style: { textAlign: 'center' }}}
+                                    onChange={(event) => handleFirst(event.target.value, i)}
                                 />
                                 -
                                 <TextField
@@ -33,8 +52,11 @@ const BetList = state => {
                                     type="number"
                                     id="bet"
                                     inputProps={{min: 0, style: { textAlign: 'center' }}}
+                                    onChange={(event) => handleSecond(event.target.value, i)}
                                 />
+                                <a>
                                 {state.state.location.state.matches[i].home}
+                                </a>
                             </div>
                         </form>
                     </Grid>
@@ -45,7 +67,19 @@ const BetList = state => {
     };
 
     return (
-        <div key={'listGroups'}>{listGroups()}</div>
+        <div>
+            <div key={'listGroups'}>{listGroups()}</div>
+            <button
+                fullWidth
+                variant='contained'
+                color='primary'
+                className={styles.submit}
+                onClick={() => console.log(firstBets, secondBets)}
+            >
+                Tallenna
+            </button>
+        </div>
+
     )
 };
 
@@ -56,6 +90,9 @@ const useStyles = makeStyles(theme => ({
         '& .MuiTextField-root': {
             maxWidth: 100,
         },
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
     },
 }));
 
