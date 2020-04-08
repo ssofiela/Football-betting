c0d4046f03e84513dd1d95ed09ed40c2293c3658
Sofia Kimpimäki <kimpims@aalto.fi>
2020-04-08 22:07:57 +0300

    Authentication fix

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -8,24 +8,34 @@ import {
     withRouter, BrowserRouter as Router
 } from 'react-router-dom';
 import GroupPage from './pages/GroupPage';
-import StatsPage from './pages/StatsPage';
 import BetPage from './pages/BetPage';
+import StatsPage from './pages/StatsPage';
+import MatchScorePage from './pages/MatchScorePage';
 import {withFirebase} from "./components/Firebase";
 import TopBar from "./components/TopBar";
 import BottomNav from "./components/BottomNav";
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Grid from '@material-ui/core/Grid';
 
 export default function AppRouter(props) {
-    const [userAuth, setAuth] = useState(false);
 
+    const [userAuth, setAuth] = useState(false);
+    const [indicator, setIndicator] = useState(true);
+
+    // Check authentication
     useEffect(() => {
+        //setIndicator(true);
         props.firebase.auth.onAuthStateChanged(function (user) {
             setAuth(user);
+            setIndicator(false);
         });
     }, []);
 
+    // If user is not authenticated, she/he can only log in/register
     const authCheck = () => {
         let value = [];
         if (!userAuth) {
+
             value.push(
                 <Switch key='notAuth'>
                     <Route key="register" exact path="/register" component={Register} />
@@ -37,6 +47,7 @@ export default function AppRouter(props) {
             value.push(
                 <Switch key='auth'>
                     <Route key="stats" exact path="/tulokset" component={StatsPage} />
+                    <Route key="match" exact path="/matsi" component={MatchScorePage} />
                     <Route key="bet" exact path="/veikkaa" component={BetPage} />
                     <Route key="home" exact path="/" component={GroupPageWithNav} />
                     <Redirect to='/'/>
@@ -46,13 +57,25 @@ export default function AppRouter(props) {
         return value;
     };
 
-        return (
-            <Router>
-                <TopBarWithRouter />
-                {authCheck()}
-                <BottomNavWithRouter />
-            </Router>
-        );
+    return (
+
+        <Router>
+            <TopBarWithRouter/>
+            {indicator ?
+                <div>
+                    <Grid container justify='center'
+                          alignItems='center'>
+                        <Grid item>
+                            <CircularProgress color='secondary'/>
+                        </Grid>
+                    </Grid>
+                </div>
+                :
+                authCheck()
+            }
+        </Router>
+    );
+
 }
 
 const GroupPageWithNav = withRouter(GroupPage);
